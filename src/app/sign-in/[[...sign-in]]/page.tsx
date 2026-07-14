d604af940ea74388c437050cd6a3d89aa576a270
Apotos <simeonbusari@gmail.com>
2026-07-14 13:14:32 +0300

    chore: hide demo account card on sign-in page

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -1,12 +1,12 @@
 import { SignIn } from "@clerk/nextjs";
 
-import { DemoLoginButton } from "@/components/marketing/demo-login-button";
+// import { DemoLoginButton } from "@/components/marketing/demo-login-button";
 
 export default function SignInPage() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center gap-6 p-6">
       <SignIn />
-      <DemoLoginButton />
+      {/* <DemoLoginButton /> */}
     </div>
   );
 }
